3dd0e3d1df45a5eda194ce96e6f67f335e29e504
tomicervillera <cervilleratomas@outlook.com>
2021-12-20 04:03:41 -0300

    Feat: Muestra de desarrollador en detalles de juego. Nota: plantilla temporal

--- src/app/modules/games/modules/details/components/details/details.component.ts
+++ src/app/modules/games/modules/details/components/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Juego } from '@app/models/api-models';
+import { DesarrolladoresService } from '@app/services/desarrolladores.service';
 import { JuegosService } from '@app/services/juegos.service';
 
 @Component({
@@ -10,12 +11,33 @@ import { JuegosService } from '@app/services/juegos.service';
 })
 export class DetailsComponent implements OnInit {
   juego: any = {};
+  desarrollador: any = {};
 
-  constructor(private actRoute: ActivatedRoute, private juegosSvc: JuegosService) {
+  constructor(
+    private actRoute: ActivatedRoute,
+    private juegosSvc: JuegosService,
+    private desarrolladorSvc: DesarrolladoresService
+  ) {
+    //Obtener juego en específico
+    // this.actRoute.params.subscribe((params) => {
+    //   this.juegosSvc.getJuegoById2(params['id']).subscribe((response) => (this.juego = { ...response.data }));
+    // });
+
     this.actRoute.params.subscribe((params) => {
-      this.juegosSvc.getJuegoById2(params['id']).subscribe((response) => (this.juego = { ...response.data }));
-      console.log(this.juego);
+      this.juegosSvc.getJuegoById2(params['id']).subscribe((response) => {
+        this.juego = { ...response.data };
+        this.desarrolladorSvc
+          .getDesarrolladorById(this.juego.idDesarrollador)
+          .subscribe((response) => (this.desarrollador = { ...response.data }));
+      });
     });
+
+    //Obtener desarrollador asociado
+
+    console.log(this.juego.idDesarrollador);
+    // this.desarrolladorSvc
+    //   .getDesarrolladorById(this.juego.idDesarrollador)
+    //   .subscribe((response) => (this.desarrollador = { ...response.data }));
   }
 
   ngOnInit(): void {}
